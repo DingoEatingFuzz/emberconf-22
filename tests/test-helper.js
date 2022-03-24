d5f9dd884bc146021226292a91a5eb489a6ef208
Michael Lange <dingoeatingfuzz@gmail.com>
2022-03-24 16:39:13 -0700

    Let's get going

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'emberconf-22/app';
+import config from 'emberconf-22/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
